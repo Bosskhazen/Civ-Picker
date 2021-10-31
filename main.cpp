f061eb9cc0dad9e1b23d020d2c4bd964adec93e1
Bosskhazen <bouarfa.amine.finance@gmail.com>
2021-10-31 11:24:34 +0100

    Adding civ 3

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 unsigned int opusChoice(); // Choosing the right Civ game
 int randomPicking(vector<string> const&); //picking a random leader
-void result (vector<string> const&, int const&); // announcing the results
+void print_result (vector<string> const&, int const&); // announcing the results
 
 int main()
 {
@@ -29,7 +29,7 @@ int main()
             while(getline(ist, line)){
                 leaders.push_back(line);
             }
-            result(leaders, randomPicking(leaders));
+            print_result(leaders, randomPicking(leaders));
             } break;
         case (2) : {
             //Opening and testing the opening of the civ list file
@@ -40,10 +40,19 @@ int main()
             while(getline(ist, line)){
                 leaders.push_back(line);
             }
-            result(leaders, randomPicking(leaders));
+            print_result(leaders, randomPicking(leaders));
             } break;
-        case (3) :
-            break;
+        case (3) : {
+            //Opening and testing the opening of the civ list file
+            ifstream ist {"civ3_list.txt"};
+            if(!ist) cout << "ERROR : can't open file";
+            //storing the file in a vector
+            string line;
+            while(getline(ist, line)){
+                leaders.push_back(line);
+            }
+            print_result(leaders, randomPicking(leaders));
+            } break;
         case (4) :
             break;
         case (5) :
@@ -57,7 +66,7 @@ int main()
             while(getline(ist, line)){
                 leaders.push_back(line);
             }
-            result(leaders, randomPicking(leaders));
+            print_result(leaders, randomPicking(leaders));
             } break;
     }
 
@@ -84,8 +93,8 @@ int randomPicking (vector<string> const& l){
     return leader;
 }
 
-void result (vector<string> const& l, int const& r){
-    cout<< "Picked leader : " << l[r-1];
+void print_result (vector<string> const& l, int const& r){
+    cout<< "Picked leader/civilization : " << l[r-1];
     cout<< endl;
 
 }
